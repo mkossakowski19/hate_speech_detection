58dd0fab0c67fa2d88d1e6644ebfa0ede6df537a
Mariusz Kossakowski <44444134+mkossakowski19@users.noreply.github.com>
2023-03-03 19:45:31 +0100

    Add FastAPI components and tests

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+from src.data_models import InputRequest, Response
+from src.load_models import load_models
+from src.text_preprocessor import TextPreprocessor
+
+CLASS_MAPPING = {
+    0: "non-harmful",
+    1: "cyberbullying",
+    2: "hate-speech",
+}
+
+app = FastAPI()
+
+model, vectorizer = load_models("models")
+preprocessor = TextPreprocessor()
+
+
+@app.post("/detect-hate-speech")
+def get_prediction(input: InputRequest) -> Response:
+    text = input.text
+    text_preprocessed = preprocessor.preprocess(text)
+    text_vectorized = vectorizer.transform([text_preprocessed])
+    prediction = model.predict(text_vectorized)[0]
+    return Response(predicted_class=CLASS_MAPPING[prediction])
